1b1d2777179ef5ac6918122a9d49636df979ec2f
itrauco <chris@trau.co>
2025-04-17 22:00:18 -0400

    feat: back of original files

--- scripts/orig.sync-artifacts_2025-04-17_21-58.sh
+++ scripts/orig.sync-artifacts_2025-04-17_21-58.sh
@@ -1,7 +1,3 @@
-# NEW VERSION - Original backed up to: orig.sync-artifacts_2025-04-17_21-58.sh 
-# Version date: Thu Apr 17 09:58:41 PM EDT 2025 
-# Git branch: develop 
-# Last commit: feat: add .gitignore 
 # Original file: sync-artifacts.sh 
 # Version date: Thu Apr 17 09:58:41 PM EDT 2025 
 # Git branch: develop 
